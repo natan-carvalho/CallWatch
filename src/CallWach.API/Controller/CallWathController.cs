e21e3ed9d4060b37f66a6960855570c33be44ff9
natan-carvalho <natan.carvalho123@hotmail.com>
2025-11-02 00:09:13 -0300

    :chore: down: removed Twilio

--- src/CallWach.API/Controller/CallWathController.cs
+++ src/CallWach.API/Controller/CallWathController.cs
@@ -8,9 +8,6 @@ using CallWatch.Domain.Repositories;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-using Twilio;
-using Twilio.Rest.Api.V2010.Account;
-using Twilio.Types;
 
 namespace CallWach.API.Controller;
 
@@ -22,84 +19,77 @@ public class CallWathController(
   IMessageSender messageSender
   )
 {
-  private readonly ILoginUseCase _loginUseCase = loginUseCase;
-  private readonly IGetAllCallsUseCase _getAllCallsUseCase = getAllCallsUseCase;
-  private readonly IGetCallInfoUseCase _getCallInfoUseCase = getCallInfoUseCase;
-  private readonly IUsersReadOnlyRepository _userRepository = userRepository;
-  private readonly IMessageSender _messageSender = messageSender;
-  private const string BASEURL = "https://gestaox.aec.com.br/Chamados/AtividadesChamadosV2";
-  private const int TimerValue = 5;
-  private readonly ChromeOptions _options = new();
+    private readonly ILoginUseCase _loginUseCase = loginUseCase;
+    private readonly IGetAllCallsUseCase _getAllCallsUseCase = getAllCallsUseCase;
+    private readonly IGetCallInfoUseCase _getCallInfoUseCase = getCallInfoUseCase;
+    private readonly IUsersReadOnlyRepository _userRepository = userRepository;
+    private readonly IMessageSender _messageSender = messageSender;
+    private const string BASEURL = "https://gestaox.aec.com.br/Chamados/AtividadesChamadosV2";
+    private const int TimerValue = 5;
+    private readonly ChromeOptions _options = new();
 
-  public void Execute()
-  {
-    const string accountSid = "ACcc2aaaa253ac7797e0353b56c640067f";
-    const string authToken = "7979baef426c4da8412c6833d857a370";
-    const string phoneFrom = "whatsapp:+14155238886";
-    var phoneTo = "whatsapp:+5582996086951";
-    TwilioClient.Init(accountSid, authToken);
+    public void Execute()
+    {
+        const string accountSid = "ACcc2aaaa253ac7797e0353b56c640067f";
+        const string authToken = "7979baef426c4da8412c6833d857a370";
+        const string phoneFrom = "whatsapp:+14155238886";
+        var phoneTo = "whatsapp:+5582996086951";
 
-    // var messageOptions = new CreateMessageOptions(
-    //   new PhoneNumber("whatsapp:+558296086951"));
-    // messageOptions.From = new PhoneNumber("whatsapp:+14155238886");
-    // messageOptions.ContentSid = "HXb5b62575e6e4ff6129ad7c8efe1f983e";
-    // messageOptions.Body = "Aló Testando";
+        // var messageOptions = new CreateMessageOptions(
+        //   new PhoneNumber("whatsapp:+558296086951"));
+        // messageOptions.From = new PhoneNumber("whatsapp:+14155238886");
+        // messageOptions.ContentSid = "HXb5b62575e6e4ff6129ad7c8efe1f983e";
+        // messageOptions.Body = "Aló Testando";
 
-    var message = MessageResource.Create(
-      from: new PhoneNumber(phoneFrom),
-      to: new PhoneNumber(phoneTo),
-      body: "Só testando"
-    );
-    Console.WriteLine(message.Body);
-    // _options.AddArgument("--start-maximized");
-    // _options.AddArgument("--headless");
-    // _options.AddArgument("--disable-gpu");
-    // using var driver = new ChromeDriver(_options);
+        // _options.AddArgument("--start-maximized");
+        // _options.AddArgument("--headless");
+        // _options.AddArgument("--disable-gpu");
+        // using var driver = new ChromeDriver(_options);
 
-    // var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        // var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-    // try
-    // {
-    //   driver.Navigate().GoToUrl(BASEURL);
-    //   var title = driver.Title;
+        // try
+        // {
+        //   driver.Navigate().GoToUrl(BASEURL);
+        //   var title = driver.Title;
 
-    //   _loginUseCase.Execute("natanael.santos", "Na118629*963,", wait);
+        //   _loginUseCase.Execute("natanael.santos", "Na118629*963,", wait);
 
-    //   if (title == "Bem vindo ao sistema GestaoX - Service Desk")
-    //   {
-    //     driver.Navigate().GoToUrl(BASEURL);
+        //   if (title == "Bem vindo ao sistema GestaoX - Service Desk")
+        //   {
+        //     driver.Navigate().GoToUrl(BASEURL);
 
-    //     while (true)
-    //     {
-    //       Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificando chamados...");
+        //     while (true)
+        //     {
+        //       Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificando chamados...");
 
-    //       foreach (var row in _getAllCallsUseCase.Execute(wait))
-    //       {
-    //         var cells = row.FindElements(By.TagName("td"));
-    //         if (cells.Count > 0)
-    //         {
-    //           var callInfo = _getCallInfoUseCase.Execute(cells);
-    //           var validatePercentage = CallAggregate.ValidatePercentage(callInfo.Percentage);
+        //       foreach (var row in _getAllCallsUseCase.Execute(wait))
+        //       {
+        //         var cells = row.FindElements(By.TagName("td"));
+        //         if (cells.Count > 0)
+        //         {
+        //           var callInfo = _getCallInfoUseCase.Execute(cells);
+        //           var validatePercentage = CallAggregate.ValidatePercentage(callInfo.Percentage);
 
-    //           if (validatePercentage && callInfo.Status != "PENDENTE USUÁRIO")
-    //           {
-    //             var user = await _userRepository.GetByName(callInfo.Responsible.ToLower());
-    //             _messageSender.Send(user!.Number, callInfo.Requester, callInfo.Service, callInfo.Service);
-    //           }
-    //         }
-    //       }
+        //           if (validatePercentage && callInfo.Status != "PENDENTE USUÁRIO")
+        //           {
+        //             var user = await _userRepository.GetByName(callInfo.Responsible.ToLower());
+        //             _messageSender.Send(user!.Number, callInfo.Requester, callInfo.Service, callInfo.Service);
+        //           }
+        //         }
+        //       }
 
-    //       Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificação concluída. Aguardando próximo ciclo...");
-    //       await Task.Delay(TimeSpan.FromMinutes(TimerValue));
-    //       driver.Navigate().GoToUrl(BASEURL);
-    //     }
-    //   }
-    //   Console.WriteLine($"Título da página: {title}");
-    //   Console.ReadLine();
-    // }
-    // catch (Exception ex)
-    // {
-    //   Console.WriteLine("Ocorreu um erro: " + ex.Message);
-    // }
-  }
+        //       Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificação concluída. Aguardando próximo ciclo...");
+        //       await Task.Delay(TimeSpan.FromMinutes(TimerValue));
+        //       driver.Navigate().GoToUrl(BASEURL);
+        //     }
+        //   }
+        //   Console.WriteLine($"Título da página: {title}");
+        //   Console.ReadLine();
+        // }
+        // catch (Exception ex)
+        // {
+        //   Console.WriteLine("Ocorreu um erro: " + ex.Message);
+        // }
+    }
 }
