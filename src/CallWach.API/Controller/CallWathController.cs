85e8f969aebaebc81ccbcf817093ffd59cd61510
natan-carvalho <natan.carvalho123@hotmail.com>
2025-11-02 18:46:07 -0300

    :wrench: chore: remove Twilio

--- src/CallWach.API/Controller/CallWathController.cs
+++ src/CallWach.API/Controller/CallWathController.cs
@@ -1,4 +1,3 @@
-using System.Drawing;
 using CallWach.Domain.Aggregates;
 using CallWatch.Application.UseCases.GetAllCalls;
 using CallWatch.Application.UseCases.GetCallInfo;
@@ -19,67 +18,66 @@ public class CallWathController(
   IMessageSender messageSender
   )
 {
-    private readonly ILoginUseCase _loginUseCase = loginUseCase;
-    private readonly IGetAllCallsUseCase _getAllCallsUseCase = getAllCallsUseCase;
-    private readonly IGetCallInfoUseCase _getCallInfoUseCase = getCallInfoUseCase;
-    private readonly IUsersReadOnlyRepository _userRepository = userRepository;
-    private readonly IMessageSender _messageSender = messageSender;
-    private const string BASEURL = "https://gestaox.aec.com.br/Chamados/AtividadesChamadosV2";
-    private const int TimerValue = 5;
-    private readonly ChromeOptions _options = new();
+  private readonly ILoginUseCase _loginUseCase = loginUseCase;
+  private readonly IGetAllCallsUseCase _getAllCallsUseCase = getAllCallsUseCase;
+  private readonly IGetCallInfoUseCase _getCallInfoUseCase = getCallInfoUseCase;
+  private readonly IUsersReadOnlyRepository _userRepository = userRepository;
+  private readonly IMessageSender _messageSender = messageSender;
+  private const string BASEURL = "https://gestaox.aec.com.br/Chamados/AtividadesChamadosV2";
+  private const int TimerValue = 5;
+  private readonly ChromeOptions _options = new();
 
-    public async Task Execute()
+  public async Task Execute()
+  {
+    _options.AddArgument("--start-maximized");
+    _options.AddArgument("--headless");
+    _options.AddArgument("--disable-gpu");
+    using var driver = new ChromeDriver(_options);
+
+    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+    try
     {
+      driver.Navigate().GoToUrl(BASEURL);
+      var title = driver.Title;
 
-        _options.AddArgument("--start-maximized");
-        _options.AddArgument("--headless");
-        _options.AddArgument("--disable-gpu");
-        using var driver = new ChromeDriver(_options);
+      _loginUseCase.Execute("natanael.santos", "Na118629*963,", wait);
 
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+      if (title == "Bem vindo ao sistema GestaoX - Service Desk")
+      {
+        driver.Navigate().GoToUrl(BASEURL);
 
-        try
+        while (true)
         {
-            driver.Navigate().GoToUrl(BASEURL);
-            var title = driver.Title;
+          Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificando chamados...");
 
-            _loginUseCase.Execute("natanael.santos", "Na118629*963,", wait);
-
-            if (title == "Bem vindo ao sistema GestaoX - Service Desk")
+          foreach (var row in _getAllCallsUseCase.Execute(wait))
+          {
+            var cells = row.FindElements(By.TagName("td"));
+            if (cells.Count > 0)
             {
-                driver.Navigate().GoToUrl(BASEURL);
+              var callInfo = _getCallInfoUseCase.Execute(cells);
+              var validatePercentage = CallAggregate.ValidatePercentage(callInfo.Percentage);
 
-                while (true)
-                {
-                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificando chamados...");
-
-                    foreach (var row in _getAllCallsUseCase.Execute(wait))
-                    {
-                        var cells = row.FindElements(By.TagName("td"));
-                        if (cells.Count > 0)
-                        {
-                            var callInfo = _getCallInfoUseCase.Execute(cells);
-                            var validatePercentage = CallAggregate.ValidatePercentage(callInfo.Percentage);
-
-                            if (validatePercentage && callInfo.Status != "PENDENTE USUÁRIO")
-                            {
-                                var user = await _userRepository.GetByName(callInfo.Responsible.ToLower());
-                                _messageSender.Send(user!.Number, callInfo.Requester, callInfo.Service, callInfo.Service);
-                            }
-                        }
-                    }
-
-                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificação concluída. Aguardando próximo ciclo...");
-                    await Task.Delay(TimeSpan.FromMinutes(TimerValue));
-                    driver.Navigate().GoToUrl(BASEURL);
-                }
+              if (validatePercentage && callInfo.Status != "PENDENTE USUÁRIO")
+              {
+                var user = await _userRepository.GetByName(callInfo.Responsible.ToLower());
+                _messageSender.Send(user!.Number, callInfo.Requester, callInfo.Service, callInfo.Service);
+              }
             }
-            Console.WriteLine($"Título da página: {title}");
-            Console.ReadLine();
+          }
+
+          Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Verificação concluída. Aguardando próximo ciclo...");
+          await Task.Delay(TimeSpan.FromMinutes(TimerValue));
+          driver.Navigate().GoToUrl(BASEURL);
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Ocorreu um erro: " + ex.Message);
-        }
+      }
+      Console.WriteLine($"Título da página: {title}");
+      Console.ReadLine();
     }
+    catch (Exception ex)
+    {
+      Console.WriteLine("Ocorreu um erro: " + ex.Message);
+    }
+  }
 }
